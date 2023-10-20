d3f7f24d7ec7c1762bca915396492f239a70faf6
Willie Northway <willn@umich.edu>
2023-10-20 18:28:22 -0400

    Move web assets into the public directory.

--- push_all_to_production.sh
+++ push_all_to_production.sh
@@ -1,5 +1,5 @@
 
-rsync -e 'ssh -p 1022' -avz . gocoho@gocoho.org:/home/gocoho/public_html/go_workday_kanban/
+rsync -e 'ssh -p 1022' -avz public/ gocoho@gocoho.org:/home/gocoho/public_html/go_workday_kanban/
 
 #reset permissions
 ssh -i ~/.ssh/id_dsa -p 1022 gocoho@gocoho.org 'cd ~/public_html/go_workday_kanban/ && ~/bin/fix_web_perms.sh';
