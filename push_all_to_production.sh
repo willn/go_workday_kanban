8c39662c359c53c1caa3e6bea93c2ed2ef89529d
Willie Northway <willn@umich.edu>
2023-10-26 21:41:30 -0400

    Correct the path

--- push_all_to_production.sh
+++ push_all_to_production.sh
@@ -1,7 +1,7 @@
 
-rsync -e 'ssh -p 1022' -avz public/ gocoho@gocoho.org:/home/gocoho/public_html/go_workday_kanban/
+rsync -e 'ssh -p 1022' -avz public/ gocoho@gocoho.org:/home/gocoho/public_html/workday_kanban/
 
 #reset permissions
-ssh -i ~/.ssh/id_dsa -p 1022 gocoho@gocoho.org 'cd ~/public_html/go_workday_kanban/ && ~/bin/fix_web_perms.sh';
+ssh -i ~/.ssh/id_dsa -p 1022 gocoho@gocoho.org 'cd ~/public_html/workday_kanban/ && ~/bin/fix_web_perms.sh';
 
 
